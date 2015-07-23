6f688f0c77f45fa91cc4e28b5840528886c4e119
Joey Liechty <joey.liechty@unt.edu>
2015-07-23 15:29:15 -0500

    inital commit, needs some styling and remainder of formats

---
+++ discogs_format_to_wantlist.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @require http://code.jquery.com/jquery-latest.js
+// @name         Add all format to wantlist
+// @namespace    http://dollardialup.com/
+// @version      0.1
+// @description  adds buttons to discogs master release page for adding all of a certain format to your wantlist
+// @author       Joey Liechty, Scott Powers
+// @match        http://www.discogs.com/*/master/*
+// @grant        none
+// ==/UserScript==
+
+function Add_Format($albumformat)
+{
+    $("#main_wrapper #page #versions tr td span.format").each(function(){
+
+        if($(this).text().indexOf($albumformat) > -1)
+        {
+            $(this).closest('tr').find('td.actions li.add_to_wantlist').trigger("mouseover").trigger("click");
+        }
+    });
+}
+
+
+$(document).ready(function() {
+    $("div.collections_buttons").css("height","auto");
+
+    $('<a style="margin-top: 5px;" id="addalllp" class="button button_small">Add All LP To Want List</a>').insertAfter("#page_aside div.section_content a.want_add_all_button");
+    $("#addalllp").click(function() { Add_Format("LP") });
+
+    $('<a style="margin-top: 5px;" id="addallcd" class="button button_small">Add All CD To Want List</a>').insertAfter("#page_aside div.section_content a.want_add_all_button");
+    $("#addallcd").click(function() { Add_Format("CD") });
+
+});
